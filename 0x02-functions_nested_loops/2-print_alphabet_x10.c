7145b63670ddf9b1de5815135cd2289028bbaa9e
Chikamnele56 <chikaobinna005@gmail.com>
2022-10-21 17:48:14 -0700

    lk

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,14 @@ void print_alphabet_x10(void)
 {
 	int x;
 	int j;
-
-	while (x = 0; x < 10; x++);
 	{
-		while (j = 'a'; j <= 'z'; j++);
+		for (x = 0; x < 10; x++)
 		{
-			_putchar(j);
+			for (j = 'a'; j <= 'z'; j++)
+			{
+				_putchar(j);
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
