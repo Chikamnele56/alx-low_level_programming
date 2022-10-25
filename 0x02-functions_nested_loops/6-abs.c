c6c5ddeb81fa59c660e5f2e3cd07e197e706a097
Chikamnele56 <chikaobinna005@gmail.com>
2022-10-24 18:47:00 -0700

    kk

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,10 +7,15 @@
  */
 int _abs(int n)
 {
-	if (n < 0)
-	       n = -n;
+	{
+		if (n < 0)
+			return (n);
+	}
 	else
-return (n);
-_putchar('\n');
-return (0);
+	{
+		if (n > -n)
+			return (-n);
+	}
+	_putchar('\n');
+	return (0);
 }
