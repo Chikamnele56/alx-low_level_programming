fc16f30f4f5a44ecc2480919b7b2952fa8aa75c0
Chikamnele56 <chikaobinna005@gmail.com>
2022-10-24 17:00:11 -0700

    kl

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,10 +6,11 @@
  * Return: return 0 or 1 depending on condition
  */
 
-int test_isalpha(int c)
+int _isalpha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 	_putchar('\n');
-	return(0); or (1);
+	return(0);
+        return(1);
 
 }
---
+++ 0x02-functions_nested_loops/main_0.c
@@ -0,0 +1,3 @@
+void test_isalpha(int n);
+int test_isalpha(int c);
+test_isalpha(n);
