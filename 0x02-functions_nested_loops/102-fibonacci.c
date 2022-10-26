5ebcd0150935707bcfe564133abb81f87b2486ed
Chikamnele56 <chikaobinna005@gmail.com>
2022-10-25 17:00:49 -0700

    kk

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibronacci starting with 1 and 2
+ * followed by a new line
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	long int i, j, k, l;
+
+	j = 1;
+	k = 2;
+	for (i = 1; i <= 50; ++i)
+	{
+		if (j != 20365011074)
+		{
+			printf("%ld, ", j);
+		}
+		else
+		{
+			printf("%ld\n", j);
+		}
+		l = j + k;
+		j = k;
+		k = l;
+	}
+	return (0);
+}
