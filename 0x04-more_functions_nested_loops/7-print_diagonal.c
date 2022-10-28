918d8b249514038a74ab60ad0d61f0a7f88d00af
Chikamnele56 <chikaobinna005@gmail.com>
2022-10-28 16:56:48 -0700

    kk

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,16 +11,19 @@ void print_diagonal(int n)
 	int j;
 
 	if (n > 0)
-	{
+	{	
 		for (; i < n; i++)
 		{
-			_putchar(' ');
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
 		}
-		_putchar(92);
+	}
+	else
+	{
 		_putchar('\n');
 	}
-}
-else
-{
-	_putchar('\n');
 }
