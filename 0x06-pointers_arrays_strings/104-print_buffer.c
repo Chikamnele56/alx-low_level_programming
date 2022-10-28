2a625b24f3b1f2167c2ef7ad3323529b587b5fae
Chikamnele56 <chikaobinna005@gmail.com>
2022-10-28 15:26:52 -0700

    kk

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_line - prints a s bytes of a buffer
+ * print_line - prints as bytes of a buffer
  * @c: buffer to print
  * @s: bytes of buffer to print
  * @l: line of buffer to print
@@ -17,13 +17,15 @@ for (j = 0; j <= 9; j++)
 if (j <= s)
 printf("%02x", c[l * 10 + j]);
 else
-printf("  ");
+printf(" ");
 if (j % 2)
 putchar(' ');
 }
 for (k = 0; k <= s; k++)
 {
 if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+putchar(c[l * 10 + k]);
+else
 putchar('.');
 }
 }
