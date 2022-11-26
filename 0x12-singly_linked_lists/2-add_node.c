09abb632a8ca8423915f3f199cb5831a7b735297
Chikamnele56 <chikaobinna005@gmail.com>
2022-11-26 04:08:34 -0800

    list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,7 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 	int len;
 	list_t *new;
 
-	new_node = malloc(sizeof(list_t));
+	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
@@ -24,7 +24,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	for (len = 0; str[len];)
-		len;
+		len++;
 
 	new->str = dup;
 	new->len = len;
