378ee553e7091394842c85ca3537c8380bcd9def
Chikamnele56 <chikaobinna005@gmail.com>
2022-12-10 03:33:32 -0800

    file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -39,6 +39,7 @@ void close_file(int fd)
 	if (c == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+
 		exit(100);
 	}
 }
@@ -91,6 +92,7 @@ int main(int argc, char *argv[])
 		to = open(argv[2], O_WRONLY | O_APPEND);
 	} while
 	(r > 0);
+
 	free(buffer);
 	close_file(from);
 	close_file(to);
